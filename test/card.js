4d241b3983db0a1d9c7a4e8052f03b8677e8e4c7
Geoff Woodburn <geoff@woodburndesigns.com>
2016-09-30 14:53:05 -0700

    Updates based on pr feedback

--- test/card.js
+++ test/card.js
@@ -1,6 +1,6 @@
 import jsdom from 'mocha-jsdom';
 import sinon from 'sinon';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import { expect } from 'chai';
 import React from 'react';
 
@@ -10,34 +10,32 @@ describe('<Card />', () => {
   jsdom();
 
   it('should respond to click events if onClick provided', () => {
-    const wrapper = shallow(<Card />);
     const onClick = sinon.spy();
-    wrapper.setProps({ onClick });
-    wrapper.simulate('click', { preventDefault: () => {} });
+    const wrapper = mount(<Card onClick={ onClick } />);
+
+    wrapper.simulate('click');
 
     expect(onClick.called).to.equal(true);
   });
 
   it('should respond to remove link if onRemove provided', () => {
-    const wrapper = shallow(<Card />);
     const onRemove = sinon.spy();
-    wrapper.setProps({ onRemove });
-    wrapper.find('.rui-card-remove').simulate('click', { preventDefault: () => {} });
+    const wrapper = mount(<Card onRemove={ onRemove } />);
+
+    wrapper.find('.rui-card-remove').simulate('click');
 
     expect(onRemove.called).to.equal(true);
   });
 
   it('should accept a classname prop', () => {
-    const wrapper = shallow(<Card />);
-    wrapper.setProps({ className: 'cards-rule' });
+    const wrapper = shallow(<Card className="cards-rule" />);
 
-    expect(wrapper.find('.cards-rule')).to.have.length(1);
+    expect(wrapper.hasClass('cards-rule'));
   });
 
   it('should accept a selected prop', () => {
-    const wrapper = shallow(<Card />);
-    wrapper.setProps({ selected: true });
+    const wrapper = shallow(<Card selected />);
 
-    expect(wrapper.find('.rui-card-selected')).to.have.length(1);
+    expect(wrapper.hasClass('.rui-card-selected'));
   });
 });
