2d1385867cbab94cf287320961373d50a132cf0a
Geoff Woodburn <geoff@woodburndesigns.com>
2016-10-20 13:24:09 -0700

    Improving our build processes and cleaning up some scss.

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -2,13 +2,23 @@ const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
+  devServer: {
+    publicPath: '/build/',
+    colors: true,
+    quiet: false,
+    noInfo: false,
+    inline: true,
+    lazy: false,
+  },
   entry: {
-    library: './source/index.js',
+    styleguide: [
+      'webpack-dev-server/client?http://localhost:8080',
+      './source/react/app.js',
+    ],
   },
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: '[name].js',
-    libraryTarget: 'commonjs2',
   },
   module: {
     loaders: [{
