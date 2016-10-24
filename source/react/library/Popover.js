164c9399e86e93735e2841711eee298f1eaf3b9f
Geoff Woodburn <geoff@woodburndesigns.com>
2016-10-24 16:13:33 -0700

    Lets just use the ref of the current element rather than finding the dom node.

--- source/react/library/Popover.js
+++ source/react/library/Popover.js
@@ -43,9 +43,7 @@ class Popover extends React.Component {
   }
 
   setPosition() {
-    /* eslint-disable react/no-find-dom-node */
-    const el = ReactDOM.findDOMNode(this);
-    /* eslint-enable react/no-find-dom-node */
+    const el = this.elem;
     const elPosition = el.getBoundingClientRect();
 
     this.setState({
@@ -65,7 +63,7 @@ class Popover extends React.Component {
     }
 
     return (
-      <div style={ { display: 'inline-block' } }>
+      <div style={ { display: 'inline-block' } } ref={ (c) => { this.elem = c; } }>
         <Portal closeOnOutsideClick onClose={ this.onClose } openByClickOn={ this.props.target }>
           <div className={ className } style={ styles }>
             { this.props.children }
