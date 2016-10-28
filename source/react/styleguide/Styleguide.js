5421006df76fa173f1e81bc30ef762c170956ad7
Geoff Woodburn <geoff@woodburndesigns.com>
2016-10-28 11:22:03 -0700

    Icon's now available within our button component.

--- source/react/styleguide/Styleguide.js
+++ source/react/styleguide/Styleguide.js
@@ -4,6 +4,7 @@ import Card from '../library/Card';
 import Loading from '../library/LoadingIndicator';
 import Button from '../library/Button';
 import Icon from '../library/Icon';
+import icons from '../library/icons';
 import Dropdown from '../library/dropdown/Dropdown';
 import DropdownMenu from '../library/dropdown/DropdownMenu';
 
@@ -24,18 +25,37 @@ class Styleguide extends React.Component {
     return <DropdownMenu width="200px" target={ button } options={ options } />;
   }
 
+  renderIcons() {
+    const iconKeys = Object.keys(icons);
+
+    return iconKeys.map(key => (
+      <div className="icon-block">
+        <Icon type={ key } />
+        <div className="icon-block-title">{ key }</div>
+      </div>
+    ));
+  }
+
   render() {
     const page = this.state;
     const popover = this.renderPopover();
+    const icons = this.renderIcons();
 
     return (
       <div>
         <Sidebar />
         <div className="content">
           <Loading />
-          <Card title="hello world!" />
-          <Icon type="loader" />
-          <Button label="Bar" />
+          <div className="section">
+            <Card title="hello world!" />
+          </div>
+          <div className="section">
+            <Button label="Bar" />
+            <Button icon="plus" label="Add a thing" />
+          </div>
+          <div className="section">
+            { icons }
+          </div>
         </div>
         { popover }
         <Dropdown
