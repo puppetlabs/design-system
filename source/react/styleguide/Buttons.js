5cb4a29499af792d803a2a4b05e51015ed9e9f24
Geoff Woodburn <geoff@woodburndesigns.com>
2016-12-08 11:39:54 -0800

    Fix to floating action buttons.

--- source/react/styleguide/Buttons.js
+++ source/react/styleguide/Buttons.js
@@ -7,9 +7,17 @@ class Buttons extends React.Component {
     return (
       <div>
         <h1>Buttons</h1>
-        <Button label="default" />
-        <Button secondary label="secondary" />
-        <Button disabled label="disabled" />
+        <div className="sg-section">
+          <h2 className="sg-section-title">Standard Buttons</h2>
+          <Button label="default" />
+          <Button secondary label="secondary" />
+          <Button disabled label="disabled" />
+        </div>
+        <div className="sg-section">
+          <h2 className="sg-section-title">Floating Action Buttons</h2>
+          <Button floating />
+          <Button floating disabled />
+        </div>
       </div>
     );
   }
