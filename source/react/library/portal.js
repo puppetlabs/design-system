2fcc7270af43a361375220c2e86469035151993d
Geoff Woodburn <geoff@woodburndesigns.com>
2016-12-12 11:18:26 -0800

    Portal should default to being opened but have the ability to also accept a prop.

--- source/react/library/portal.js
+++ source/react/library/portal.js
@@ -2,7 +2,15 @@ import React from 'react';
 import Portal from 'react-portal';
 
 const portal = function portal(Base, opts = {}) {
-  return props => <Portal isOpened={ props.isOpened } { ...opts }><Base { ...props } /></Portal>;
+  return (props) => {
+    const isOpened = {}.hasOwnProperty.call(props, 'isOpened') ? props.isOpened : true;
+
+    return (
+      <Portal isOpened={ isOpened } { ...opts }>
+        <Base { ...props } />
+      </Portal>
+    );
+  };
 };
 
 export default portal;
