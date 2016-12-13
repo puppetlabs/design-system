77ad309c31abdc08b12a3813ef91c8798bf21e2d
Geoff Woodburn <geoff@woodburndesigns.com>
2016-12-12 16:11:29 -0800

    Linting.

--- source/react/styleguide/Dropdowns.js
+++ source/react/styleguide/Dropdowns.js
@@ -2,35 +2,28 @@ import React from 'react';
 import StyleguideSection from './partials/StyleguideSection';
 import Dropdown from '../library/dropdown/Dropdown';
 
-class Dropdowns extends React.Component {
+const Dropdowns = () => {
+  const options = [
+    { id: '1', value: 'option 1' },
+    { id: '2', value: 'option 2' },
+  ];
 
-  renderDropdown() {
-    const options = [
-      { id: '1', value: 'option 1' },
-      { id: '2', value: 'option 2' },
-    ];
+  const dropdown = (
+    <Dropdown
+      label="I'm a dropdown"
+      hint="I'm a hint"
+      options={ options }
+    />
+  );
 
-    return (
-      <Dropdown
-        label="I'm a dropdown"
-        hint="I'm a hint"
-        options={ options }
-      />
-    );
-  }
-
-  render() {
-    const dropdown = this.renderDropdown();
-
-    return (
-      <div>
-        <h1>Dropdowns</h1>
-        <StyleguideSection title="Default Dropdown">
-          { dropdown }
-        </StyleguideSection>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <h1>Dropdowns</h1>
+      <StyleguideSection title="Default Dropdown">
+        { dropdown }
+      </StyleguideSection>
+    </div>
+  );
+};
 
 export default Dropdowns;
