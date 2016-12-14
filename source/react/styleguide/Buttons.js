605cda03f3360531f4188c685789e195c445701f
Geoff Woodburn <geoff@woodburndesigns.com>
2016-12-14 15:48:00 -0800

    Updating buttons in styleguide.

--- source/react/styleguide/Buttons.js
+++ source/react/styleguide/Buttons.js
@@ -1,44 +1,30 @@
 import React from 'react';
 import Button from '../library/Button';
+import StyleguideSection from './partials/StyleguideSection';
 
-class Buttons extends React.Component {
-
-  render() {
-    return (
-      <div>
-        <h1>Buttons</h1>
-        <div className="sg-section">
-          <h2 className="sg-section-title">Standard Buttons</h2>
-
-          <div className="sg-subsection">
-            <h3 className="sg-subsection-title">Default</h3>
-            <Button label="default" />
-            <Button disabled label="disabled" />
-            <Button disabled processing label="processing" />
-          </div>
-
-          <div className="sg-subsection">
-            <h3 className="sg-subsection-title">Secondary</h3>
-            <Button secondary label="default" />
-            <Button secondary disabled label="disabled" />
-            <Button secondary disabled processing label="processing" />
-          </div>
-
-          <div className="sg-subsection">
-            <h3 className="sg-subsection-title">Transparent</h3>
-            <Button transparent label="default" />
-            <Button transparent disabled label="disabled" />
-            <Button transparent disabled processing label="processing" />
-          </div>
-        </div>
-        <div className="sg-section">
-          <h2 className="sg-section-title">Floating Action Buttons</h2>
-          <Button floating />
-          <Button floating disabled />
-        </div>
-      </div>
-    );
-  }
-}
+const Buttons = () => (
+  <div>
+    <h1>Buttons</h1>
+    <StyleguideSection title="Default Buttons">
+      <Button label="default" />
+      <Button disabled label="disabled" />
+      <Button disabled processing label="processing" />
+    </StyleguideSection>
+    <StyleguideSection title="Secondary Buttons">
+      <Button secondary label="default" />
+      <Button secondary disabled label="disabled" />
+      <Button secondary disabled processing label="processing" />
+    </StyleguideSection>
+    <StyleguideSection title="Transparent Buttons">
+      <Button transparent label="default" />
+      <Button transparent disabled label="disabled" />
+      <Button transparent disabled processing label="processing" />
+    </StyleguideSection>
+    <StyleguideSection title="Floating Action Buttons">
+      <Button floating />
+      <Button floating disabled />
+    </StyleguideSection>
+  </div>
+);
 
 export default Buttons;
