324c6e3a851836aa918cf282837714b980c0e753
Geoff Woodburn <geoff@woodburndesigns.com>
2016-12-16 16:08:32 -0800

    We should no longer be using portal's native onClose method. Instead lets rely onOutsideClick and state.

--- source/react/library/portal.js
+++ source/react/library/portal.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import Portal from 'react-portal';
 
-const portal = function portal(Base, opts = {}) {
+const portal = function portal(Base) {
   const defaultProps = { isOpened: true };
 
-  const component = props => {
-    return (
-      <Portal isOpened={ props.isOpened } onClose={ props.onClose } { ...opts }>
-        <Base { ...props } />
-      </Portal>
-    );
-  };
+  const component = props => (
+    <Portal isOpened={ props.isOpened }>
+      <Base { ...props } />
+    </Portal>
+  );
 
   component.defaultProps = defaultProps;
 
