c7c9e70b531bdc70db8c1df3197753bda4bdf1be
Colby Aley <colby@aley.me>
2017-02-24 15:21:21 -0800

    Class for dropdown with no padding, expand Menu styleguide example

--- source/react/styleguide/Menus.js
+++ source/react/styleguide/Menus.js
@@ -5,6 +5,7 @@ import Popover from '../library/Popover';
 
 import Menu from '../library/menu/Menu';
 import MenuHeader from '../library/menu/MenuHeader';
+import MenuList from '../library/menu/MenuList';
 
 class Menus extends React.Component {
   renderTarget() {
@@ -14,13 +15,19 @@ class Menus extends React.Component {
   render() {
     const target = this.renderTarget();
 
+    const listOptions = [
+      { id: 0, value: 'option1' },
+      { id: 1, value: 'option2' },
+    ];
+
     return (
       <div>
         <h1>Menus</h1>
         <StyleguideSection title="Menu In Dropdown">
-          <Popover target={ target }>
+          <Popover target={ target } className="rc-popover-no-padding">
             <Menu>
-              <MenuHeader title="I'm a happy menu!" />
+              <MenuHeader title="I'm a happy menu title!" />
+              <MenuList options={ listOptions }/>
             </Menu>
           </Popover>
         </StyleguideSection>
