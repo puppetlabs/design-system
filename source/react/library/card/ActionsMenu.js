437dd54b8b59f25d88e607f346067a87addab184
Geoff Woodburn <geoff@woodburndesigns.com>
2017-09-26 16:14:38 -0700

    Update width of icon when its a kebab

--- source/react/library/card/ActionsMenu.js
+++ source/react/library/card/ActionsMenu.js
@@ -18,9 +18,11 @@ const CardActionsMenu = (props) => {
   let menu;
 
   if (props.menuOptions.length > 0) {
+    const width = props.menuToggleIcon === 'kebab' ? 4 : 16;
+
     const target = (
       <a>
-        <Icon type={ props.menuToggleIcon } height="16" width="16" />
+        <Icon type={ props.menuToggleIcon } height="16" width={ width } />
       </a>
     );
 
