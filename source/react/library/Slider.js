b77ce5b53ac0e01f580b78f496fd52e98807e17a
Geoff Woodburn <geoff@woodburndesigns.com>
2017-12-21 11:01:26 -0800

    Given a value set the handle position of a slider

--- source/react/library/Slider.js
+++ source/react/library/Slider.js
@@ -5,12 +5,14 @@ const propTypes = {
   min: React.PropTypes.number,
   max: React.PropTypes.number,
   step: React.PropTypes.number,
+  value: React.PropTypes.number,
 };
 
 const defaultProps = {
   min: 0,
   max: 100,
   step: 1,
+  value: 0,
 };
 
 /**
@@ -25,7 +27,7 @@ class Slider extends React.Component {
 
     this.state = {
       dragging: false,
-      value: 0,
+      value: props.value,
     };
 
     this.onChange = this.onChange.bind(this);
@@ -39,6 +41,26 @@ class Slider extends React.Component {
     window.addEventListener('mouseup', this.onMouseUp);
   }
 
+  componentDidMount() {
+    if (this.state.value) {
+      const position = this.calculateHandlePosition();
+      this.setHandlePosition(position);
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.value === this.state.value) {
+      this.setState({ value: nextProps.value });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.value !== this.state.value) {
+      const position = this.calculateHandlePosition();
+      this.setHandlePosition(position);
+    }
+  }
+
   componentWillUnmount() {
     window.removeEventListener('mousemove', this.onMouseMove);
     window.removeEventListener('mouseup', this.onMouseUp);
@@ -48,15 +70,15 @@ class Slider extends React.Component {
     console.log(value);
   }
 
-  onMouseDown(e) {
+  onMouseDown() {
     this.setState({ dragging: true });
   }
 
   onMouseMove(e) {
     if (this.state.dragging) {
       let mousePos = e.pageX;
-      const sliderRect = this.slider.getBoundingClientRect();
-      const handleRect = this.handle.getBoundingClientRect();
+      const sliderRect = this.getSliderRect();
+      const handleRect = this.getHandleRect();
       const sliderStart = sliderRect.left;
       const sliderEnd = sliderRect.right;
 
@@ -68,22 +90,59 @@ class Slider extends React.Component {
         mousePos = sliderEnd;
       }
 
-      const handlePos = Math.round(mousePos - sliderStart);
+      const handlePos = mousePos - sliderStart;
       const handleOffset = handleRect.width / 2;
       const adjustedHandlePos = handlePos - handleOffset;
 
       const percentage = handlePos / endValue;
       const value = this.props.max * percentage;
 
-      this.barActive.style.width = `${adjustedHandlePos}px`;
+      this.barActive.style.width = `${handlePos}px`;
       this.handle.style.left = `${adjustedHandlePos}px`;
 
       this.onChange(value);
     }
   }
 
-  onMouseUp(e) {
+  onMouseUp() {
     this.setState({ dragging: false });
+  }
+
+  getSliderRect() {
+    return this.slider ? this.slider.getBoundingClientRect() : {};
+  }
+
+  getHandleRect() {
+    return this.handle ? this.handle.getBoundingClientRect() : {};
+  }
+
+  getSliderLength(sliderRect) {
+    const start = sliderRect.left;
+    const end = sliderRect.right;
+
+    return end - start;
+  }
+
+  setHandlePosition(position, handleRect) {
+    if (!handleRect) {
+      handleRect = this.getHandleRect();
+    }
+
+    const handleOffset = handleRect.width / 2;
+
+    this.barActive.style.width = `${position}px`;
+    this.handle.style.left = `${position - handleOffset}px`;
+  }
+
+  calculateHandlePosition() {
+    const max = this.props.max;
+    const value = this.state.value;
+    const percentage = value / max;
+    const sliderRect = this.getSliderRect();
+    const sliderLength = this.getSliderLength(sliderRect);
+    const position = sliderLength * percentage;
+
+    return position;
   }
 
   render() {
