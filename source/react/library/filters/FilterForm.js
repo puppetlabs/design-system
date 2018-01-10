ddc640792894d2438483374e5896319c045cfba2
Colby Aley <colby@aley.me>
2018-01-09 19:25:28 -0800

    Abstract FilterForm out of Filters

---
+++ source/react/library/filters/FilterForm.js
@@ -0,0 +1,160 @@
+import React from 'react';
+
+import { filterOperators } from '../../constants';
+import Form from '../Form';
+
+const propTypes = {
+  filter: React.PropTypes.object,
+  removable: React.PropTypes.bool,
+  onSubmit: React.PropTypes.func,
+  onCancel: React.PropTypes.func,
+  fields: React.PropTypes.array,
+};
+
+const defaultProps = {
+  onCancel: () => {},
+  onUpdate: () => {},
+  removable: false,
+  fields: [],
+  filter: {},
+};
+
+class FilterForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      filter: props.filter,
+    };
+
+    this.onUpdate = this.onUpdate.bind(this);
+    this.onCancel = this.onCancel.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onSubmit() {
+    this.props.onSubmit(this.state.filter);
+  }
+
+  onCancel() {
+    this.props.onCancel();
+  }
+
+  onUpdate(field, values) {
+    const value = values[field];
+    const newState = {
+      filter: this.state.filter,
+    };
+
+    switch (field) {
+      case 'filterField':
+        newState.filter.field = value.id;
+        break;
+      case 'filterOperator':
+        newState.filter.op = value.id;
+        break;
+      case 'filterValue':
+        newState.filter.value = value;
+        break;
+      case 'filterRemovable':
+        newState.filter.removable = value;
+        break;
+      default:
+    }
+
+    this.setState(newState);
+  }
+
+  getFields() {
+    const filter = this.state.filter;
+
+    return this.props.fields.map(field => ({
+      id: field,
+      label: field,
+      value: field,
+      selected: filter.field === field,
+    }));
+  }
+
+  getOperators() {
+    const filter = this.state.filter;
+
+    return filterOperators.map(op => ({
+      id: op.symbol,
+      label: op.label,
+      value: op.symbol,
+      selected: filter.op === op.symbol,
+    }));
+  }
+
+  renderRemovableField() {
+    const filter = this.state.filter;
+    let jsx;
+
+    if (this.props.removable) {
+      jsx = (
+        <Form.Field
+          value={ filter.removable }
+          type="checkbox"
+          name="filterRemovable"
+          label="removable"
+          inline
+        />
+      );
+    }
+
+    return jsx;
+  }
+
+  render() {
+    const removableField = this.renderRemovableField();
+    const operators = this.getOperators();
+    const filter = this.state.filter;
+    const fields = this.getFields();
+
+    return (
+      <Form
+        submittable
+        cancellable
+        onChange={ this.onUpdate }
+        onCancel={ this.onCancel }
+        onSubmit={ this.onSubmit }
+        size="small"
+      >
+        <Form.Field
+          type="select"
+          name="filterField"
+          label="field"
+          elementProps={ {
+            disablePortal: true,
+            options: fields,
+            placeholder: 'Choose a field...',
+          } }
+        />
+        <Form.Field
+          type="select"
+          name="filterOperator"
+          label="operation"
+          elementProps={ {
+            disablePortal: true,
+            options: operators,
+            placeholder: 'Please choose...',
+          } }
+        />
+        <Form.Field
+          type="input"
+          name="filterValue"
+          label="value"
+          value={ filter.value }
+          elementProps={ { placeholder: 'e.g. Jim, 15, etc.' } }
+        />
+        { removableField }
+      </Form>
+    );
+  }
+}
+
+FilterForm.propTypes = propTypes;
+FilterForm.defaultProps = defaultProps;
+
+export default FilterForm;
--- source/react/library/filters/Filters.js
+++ source/react/library/filters/Filters.js
@@ -2,10 +2,9 @@ import React from 'react';
 import clone from 'clone';
 
 import Filter from './FilterItem';
+import FilterForm from './FilterForm';
 
-import { filterOperators } from '../../constants';
 import Button from '../Button';
-import Form from '../form';
 import List from '../list/List';
 
 const propTypes = {
@@ -42,7 +41,6 @@ class Filters extends React.Component {
     this.onCancel = this.onCancel.bind(this);
     this.onRemove = this.onRemove.bind(this);
     this.onSubmitFilter = this.onSubmitFilter.bind(this);
-    this.onUpdateFilter = this.onUpdateFilter.bind(this);
   }
 
   onAdd(e) {
@@ -61,33 +59,7 @@ class Filters extends React.Component {
     });
   }
 
-  onUpdateFilter(field, values) {
-    const value = values[field];
-    const newState = {
-      filter: this.state.filter,
-    };
-
-    switch (field) {
-      case 'filterField':
-        newState.filter.field = value.id;
-        break;
-      case 'filterOperator':
-        newState.filter.op = value.id;
-        break;
-      case 'filterValue':
-        newState.filter.value = value;
-        break;
-      case 'filterRemovable':
-        newState.filter.removable = value;
-        break;
-      default:
-    }
-
-    this.setState(newState);
-  }
-
-  onSubmitFilter() {
-    const filter = this.state.filter;
+  onSubmitFilter(filter) {
     let newFilters = [];
 
     if (this.state.editing) {
@@ -164,79 +136,15 @@ class Filters extends React.Component {
   }
 
   renderForm() {
-    let jsx;
-    let removableField;
-    const filter = this.state.filter;
-
-    if (filter) {
-      const fields = this.props.fields.map(field => ({
-        id: field,
-        label: field,
-        value: field,
-        selected: filter.field === field,
-      }));
-      const operators = filterOperators.map(op => ({
-        id: op.symbol,
-        label: op.label,
-        value: op.symbol,
-        selected: filter.op === op.symbol,
-      }));
-
-      if (this.props.removableToggle) {
-        removableField = (
-          <Form.Field
-            value={ filter.removable }
-            type="checkbox"
-            name="filterRemovable"
-            label="removable"
-            inline
-          />
-        );
-      }
-
-      return (
-        <Form
-          submittable
-          cancellable
-          onChange={ this.onUpdateFilter }
-          onCancel={ this.onCancel }
-          onSubmit={ this.onSubmitFilter }
-          size="small"
-          key={ `${this.state.editing}-form` }
-        >
-          <Form.Field
-            type="select"
-            name="filterField"
-            label="field"
-            elementProps={ {
-              disablePortal: true,
-              options: fields,
-              placeholder: 'Choose a field...',
-            } }
-          />
-          <Form.Field
-            type="select"
-            name="filterOperator"
-            label="operation"
-            elementProps={ {
-              disablePortal: true,
-              options: operators,
-              placeholder: 'Please choose...',
-            } }
-          />
-          <Form.Field
-            type="input"
-            name="filterValue"
-            label="value"
-            value={ filter.value }
-            elementProps={ { placeholder: 'e.g. Jim, 15, etc.' } }
-          />
-          { removableField }
-        </Form>
-      );
-    }
-
-    return jsx;
+    return (
+      <FilterForm
+        removable={ this.props.removableToggle }
+        fields={ this.props.fields }
+        filter={ this.state.filter }
+        onCancel={ this.onCancel }
+        onSubmit={ this.onSubmitFilter }
+      />
+    );
   }
 
   render() {
