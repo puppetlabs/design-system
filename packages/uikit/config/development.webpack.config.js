d82e86b1ef6535c49c22c54273832be5aa75edc4
Noah Muldavin <nmuldavin@gmail.com>
2018-02-15 17:24:12 -0800

    Implement basic dev server

---
+++ packages/uikit/config/development.webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/client/index.js',
+  plugins: [
+    new CleanWebpackPlugin(['dist'], { root: path.resolve(__dirname, '..') }),
+    new HtmlWebpackPlugin({
+      title: 'Example App'
+    })
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, '..', 'dist'),
+    publicPath: '/'
+  }
+}
---
+++ packages/uikit/config/production.webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/client/index.js',
+  plugins: [
+    new CleanWebpackPlugin(['dist'], { root: path.resolve(__dirname, '..') }),
+    new HtmlWebpackPlugin({
+      title: 'Example App'
+    })
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, '..', 'dist')
+  }
+}
