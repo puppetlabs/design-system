6d987cfd31089871fb49811cd19c35991d961c16
Noah Muldavin <nmuldavin@gmail.com>
2018-02-16 15:35:16 -0800

    Add postcss cssnext loaders and config

--- packages/uikit/config/common.webpack.config.js
+++ packages/uikit/config/common.webpack.config.js
@@ -31,6 +31,17 @@ module.exports = {
         use: 'babel-loader',
         exclude: /node_modules/,
       },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: { modules: true, importLoaders: 1 },
+          },
+          'postcss-loader',
+        ],
+      },
     ],
   },
 };
--- packages/uikit/src/client/index.jsx
+++ packages/uikit/src/client/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
+import styles from './index.css';
 
-const App = () => <div>I am a div</div>;
+const App = () => <div className={styles.test}>I am a div</div>;
 
 render(<App />, document.getElementById('root'));
