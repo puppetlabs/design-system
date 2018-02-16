6f8917d342c42b87f447b81a06d8a9c42165ffbb
Noah Muldavin <nmuldavin@gmail.com>
2018-02-15 16:16:32 -0800

    Set up most basic webpack build config

---
+++ packages/uikit/src/client/index.js
@@ -0,0 +1,8 @@
+const component = () => {
+  const element = document.createElement('div');
+  element.innerHTML = 'Hello webpack';
+
+  return element;
+}
+
+document.body.appendChild(component());
