c1c49dda24e26aaa83c5d8e2bbe4a2c015a3390b
Noah Muldavin <nmuldavin@gmail.com>
2018-02-19 11:46:05 -0800

    Add local name to moduleized classnames, add base global styles

--- packages/uikit/config/common.webpack.config.js
+++ packages/uikit/config/common.webpack.config.js
@@ -36,7 +36,11 @@ module.exports = {
           'style-loader',
           {
             loader: 'css-loader',
-            options: { modules: true, importLoaders: 1 },
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: '[local]--[hash:base64:5]',
+            },
           },
           'postcss-loader',
         ],
