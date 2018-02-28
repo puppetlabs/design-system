67bfd77fe42756e933a3a822797a5f57803d8d0a
Sebastian <sprokuski@gmail.com>
2018-02-27 17:14:47 -0800

    Install primary tag styles and example

--- source/react/library/tag/Tag.js
+++ source/react/library/tag/Tag.js
@@ -5,6 +5,7 @@ import { TooltipHoverArea } from '../tooltips/Tooltip';
 
 const propTypes = {
   /** Selected state */
+  primary: React.PropTypes.bool,
   selected: React.PropTypes.bool,
   className: React.PropTypes.string,
   round: React.PropTypes.bool,
@@ -18,6 +19,7 @@ const propTypes = {
 };
 
 const defaultProps = {
+  primary: false,
   selected: false,
   round: false,
   className: '',
@@ -116,9 +118,10 @@ class Tag extends React.Component {
   }
 
   render() {
-    const { onRemove, onClick, selected, size, block, round } = this.props;
+    const { onRemove, onClick, primary, selected, size, block, round } = this.props;
 
     const className = classnames('rc-tag', {
+      'rc-tag-primary': primary,
       'rc-tag-selected': selected,
       'rc-tag-selectable': onClick,
       'rc-tag-removable': onRemove,
