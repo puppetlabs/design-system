1852ed15723a4485d38354f8cfba1d3c28c8596c
Noah Muldavin <nmuldavin@gmail.com>
2018-03-15 10:33:56 -0700

    Bring in bazel and configure to run server
    Use bazelbuild/rules_nodejs. As a first step, configure rule to run server with a node binary with node modules installed through normal node toolchain

--- packages/uikit/src/server/index.js
+++ packages/uikit/src/server/index.js
@@ -1,13 +1,13 @@
-const webpack = require('webpack');
-const middleware = require('webpack-dev-middleware');
+// const webpack = require('webpack');
+// const middleware = require('webpack-dev-middleware');
 const express = require('express');
 
-const webpackConfig = require('../../config/development.webpack.config.js');
+// const webpackConfig = require('../../config/development.webpack.config.js');
 
-const compiler = webpack(webpackConfig);
+// const compiler = webpack(webpackConfig);
 
 const app = express();
 
-app.use(middleware(compiler, { publicPath: webpackConfig.output.publicPath }));
+// app.use(middleware(compiler, { publicPath: webpackConfig.output.publicPath }));
 
 app.listen(3000, () => console.log('App listening at port 3000'));
