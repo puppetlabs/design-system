59c2c5c6f210adecd9ca2bcf2705be50847f9470
Sebastian <sprokuski@gmail.com>
2018-03-20 14:05:29 -0700

    Update blank slate component to accomodate new template and images

--- source/react/library/blankSlate/BlankSlate.js
+++ source/react/library/blankSlate/BlankSlate.js
@@ -4,12 +4,16 @@ import Icon from '../icon/Icon';
 const propTypes = {
   /** Visual aide for the user */
   icon: React.PropTypes.string,
+  image: React.PropTypes.string,
+  alt: React.PropTypes.string,
   /** Textual aide for the user */
   message: React.PropTypes.any,
 };
 
 const defaultProps = {
   icon: null,
+  image: null,
+  alt: null,
   message: '',
 };
 
@@ -17,16 +21,32 @@ const defaultProps = {
  * `BlankSlate` is used in list views to signify that no objects have been created yet.
  */
 
-const BlankSlate = props => (
-  <div className="rc-blank-slate">
-    <div className="rc-blank-slate-icon">
-      <Icon height="50px" width="40px" type={ props.icon } />
+const BlankSlate = (props) => {
+  let icon;
+
+  if (props.icon) {
+    icon = (
+      <div className="rc-blank-slate-icon">
+        <Icon height="50px" width="40px" type={ props.icon } />
+      </div>
+    );
+  } else if (props.image) {
+    icon = (
+      <div className="rc-blank-slate-image">
+        <img src={ props.image } alt={ props.alt } />
+      </div>
+    );
+  }
+
+  return (
+    <div className="rc-blank-slate">
+      { icon }
+      <div className="rc-blank-slate-message">
+        { props.message }
+      </div>
     </div>
-    <div className="rc-blank-slate-message">
-      { props.message }
-    </div>
-  </div>
-);
+  );
+};
 
 BlankSlate.propTypes = propTypes;
 BlankSlate.defaultProps = defaultProps;
