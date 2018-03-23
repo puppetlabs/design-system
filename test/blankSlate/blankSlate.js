03a0c59af34dcf4515c0affdba8c610d6dbdf3ef
Sebastian <sprokuski@gmail.com>
2018-03-23 11:07:30 -0700

    Add new tests for blankslate component

--- test/blankSlate/blankSlate.js
+++ test/blankSlate/blankSlate.js
@@ -1,16 +1,40 @@
 import jsdom from 'mocha-jsdom';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import { expect } from 'chai';
 import React from 'react';
+import sinon from 'sinon';
 
 import BlankSlate from '../../source/react/library/blankSlate/BlankSlate';
+
+const defaultProps = {
+  image: '/path-to-image',
+  title: 'My blankslate',
+  lead: 'A blank slate about nothing',
+  content: 'foo',
+  note: 'bar',
+};
 
 describe('<BlankSlate />', () => {
   jsdom({ skipWindowCheck: true });
 
-  it('should render without blowing up', () => {
+  it('should render the old layout without blowing up', () => {
     const wrapper = shallow(<BlankSlate icon="fields" message="Hello!" />);
 
     expect(wrapper.length).to.eql(1);
+  });
+
+  it('should render the new layout without blowing up', () => {
+    const wrapper = shallow(<BlankSlate { ...defaultProps } />);
+
+    expect(wrapper.length).to.eql(1);
+  });
+
+  it('should render a button and respond to click events for button if onClick provided', () => {
+    const onClick = sinon.spy();
+    const wrapper = mount(<BlankSlate { ...defaultProps } onClick={ onClick } />);
+
+    wrapper.find('.rc-button').simulate('click');
+
+    expect(onClick.called).to.equal(true);
   });
 });
