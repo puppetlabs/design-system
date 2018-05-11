f67953692924ec25acf89e29c02dc999ccb5696d
Noah Muldavin <nmuldavin@gmail.com>
2018-05-11 10:58:07 -0700

    Add typography composition for secondary and tertiary

--- packages/uikit/src/components/Button/index.jsx
+++ packages/uikit/src/components/Button/index.jsx
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import unboundClassNames from 'classnames/bind';
 import styles from './Button.css';
 
+console.log(styles);
+
 const classNames = unboundClassNames.bind(styles);
 
 const Button = ({ children, secondary, tertiary, className, ...props }) => (
