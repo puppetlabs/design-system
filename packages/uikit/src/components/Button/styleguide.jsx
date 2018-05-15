807cd9045a1cd680025ae71614f93285169e2217
Noah Muldavin <nmuldavin@gmail.com>
2018-05-15 14:18:50 -0700

    Move text knob to styleguide code

--- packages/uikit/src/components/Button/styleguide.jsx
+++ packages/uikit/src/components/Button/styleguide.jsx
@@ -4,34 +4,8 @@ import React from 'react';
 
 import Button from '.';
 import styles from './styleguide.css';
-import withStyleguide from '../../../styleguide/client/higherOrderComponents/withStyleguide';
-
-const getValue = path(['target', 'value']);
-
-const text = (label, fallback) => {
-  const Knob = ({ value, updateValue }) => (
-    <label htmlFor={label}>
-      {label}
-      <input
-        type="text"
-        id={label}
-        name={label}
-        value={value}
-        onChange={compose(updateValue, getValue)}
-      />
-    </label>
-  );
-
-  Knob.propTypes = {
-    value: string.isRequired,
-    updateValue: func.isRequired,
-  };
-
-  return {
-    fallback,
-    Knob,
-  };
-};
+import { withStyleguide } from '../../../styleguide/client/higherOrderComponents';
+import { text } from '../../../styleguide/client/knobs';
 
 const knobs = {
   content: text('Content', 'Action'),
