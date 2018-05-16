a2e4743965f10eb59ba0ed9f43f1f04f2a72efff
Noah Muldavin <nmuldavin@gmail.com>
2018-05-16 09:14:46 -0700

    Create landing (default) route, tweak styles)

--- packages/uikit/styleguide/client/components/App/index.jsx
+++ packages/uikit/styleguide/client/components/App/index.jsx
@@ -3,9 +3,10 @@ import { arrayOf, shape, string, element } from 'prop-types';
 import { map } from 'ramda';
 import React from 'react';
 import { hot } from 'react-hot-loader';
-import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import ComponentRoute from './ComponentRoute';
+import Landing from '../Landing';
 import SideNav from '../SideNav';
 import styles from './App.css';
 import typography from '../../../../src/styles/typography.css';
@@ -15,7 +16,10 @@ const App = ({ components }) => (
     <div className={classNames(typography.body, styles.body)}>
       <SideNav components={components} />
       <div className={styles.content}>
-        <Switch>{map(ComponentRoute, components)}</Switch>
+        <Switch>
+          {map(ComponentRoute, components)}
+          <Route component={Landing} />
+        </Switch>
       </div>
     </div>
   </Router>
