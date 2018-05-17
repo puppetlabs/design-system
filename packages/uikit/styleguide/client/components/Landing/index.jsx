129656a810075bd1717cdc2522e7e00cc8547366
Steven Hasegawa <steven@hsgws.com>
2018-05-17 11:28:11 -0700

    Added in images and styling

--- packages/uikit/styleguide/client/components/Landing/index.jsx
+++ packages/uikit/styleguide/client/components/Landing/index.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import styles from './Landing.css';
+import path from './hero.png';
 
 const Landing = () => (
-  <div className={styles.landing}>Welcome to the puppet styleguide</div>
+  <div className={styles.landing}>
+    <h1>Welcome to Puppet’s Styleguide!</h1>
+    <p>
+      Your resource for creating user interfaces consistent with the Puppet
+      principles, design language, and best practices.
+    </p>
+    <img src={path} alt="hero illustration" width="743" height="204" />
+  </div>
 );
 
 export default Landing;
--- packages/uikit/styleguide/client/components/SideNav/index.jsx
+++ packages/uikit/styleguide/client/components/SideNav/index.jsx
@@ -5,15 +5,32 @@ import { Link } from 'react-router-dom';
 
 import ComponentLink from './ComponentLink';
 import styles from './SideNav.css';
+import path from './logo.png';
 
 const SideNav = ({ components }) => (
   <div className={styles.sideNav}>
-    <Link to="/">Puppet Styleguide</Link>
+    <Link to="/">
+      <img src={path} alt="styleguide logo" width="160" height="43" />
+    </Link>
     <ul>
+      <li>
+        <Link to="/">Overview</Link>
+      </li>
+      <li>
+        Foundations
+        <ul>
+          <li>
+            <Link to="/foundations/colors">Colors</Link>
+          </li>
+        </ul>
+      </li>
       <li>
         Components
         <ul>{map(ComponentLink, components)}</ul>
       </li>
+      <li>Guidelines</li>
+      <li>Navigation</li>
+      <li>Resources</li>
     </ul>
   </div>
 );
