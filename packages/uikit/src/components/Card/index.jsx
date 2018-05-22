6f7577053f07a4ce08d5879cd00d1ed7881820e3
Steven Hasegawa <steven@hsgws.com>
2018-05-21 17:11:59 -0700

    Added in secondary card style

--- packages/uikit/src/components/Card/index.jsx
+++ packages/uikit/src/components/Card/index.jsx
@@ -3,14 +3,16 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import styles from './Card.css';
 
-const Card = ({ link, className, ...props }) => {
+const Card = ({ secondary, link, className, ...props }) => {
   const Component = link ? 'a' : 'div';
 
   return (
     <Component
       className={classNames(
         styles.card,
+        styles.primary,
         {
+          [styles.secondary]: secondary,
           [styles.link]: link,
         },
         className,
@@ -21,6 +23,7 @@ const Card = ({ link, className, ...props }) => {
 };
 
 Card.propTypes = {
+  secondary: PropTypes.bool,
   link: PropTypes.bool,
   className: PropTypes.oneOfType([
     PropTypes.string,
@@ -30,6 +33,7 @@ Card.propTypes = {
 };
 
 Card.defaultProps = {
+  secondary: false,
   link: false,
   className: '',
 };
--- packages/uikit/src/components/Card/styleguide.jsx
+++ packages/uikit/src/components/Card/styleguide.jsx
@@ -1,26 +1,36 @@
-import { bool } from 'prop-types';
+import { bool, string } from 'prop-types';
 import React from 'react';
 
 import Card from '.';
 import { withControls } from '../../../styleguide/client/higherOrderComponents';
-import { boolean } from '../../../styleguide/client/knobs';
+import { boolean, select } from '../../../styleguide/client/knobs';
+
+const typeOptions = [
+  { text: 'Primary', value: 'primary' },
+  { text: 'Secondary', value: 'secondary' },
+];
 
 const knobs = {
+  type: select('Type', typeOptions, 'primary'),
   link: boolean('Link', false),
 };
 
-const CardStyleguide = ({ link }) => (
+const CardStyleguide = ({ link, type }) => (
   <div>
     <h1>Card</h1>
-    <Card link={link}>Blah blah</Card>
+    <Card secondary={type === 'secondary'} link={link}>
+      Blah blah
+    </Card>
   </div>
 );
 
 CardStyleguide.propTypes = {
+  type: string,
   link: bool,
 };
 
 CardStyleguide.defaultProps = {
+  type: 'primary',
   link: false,
 };
 
