81a82e6b5ec50f64ea231680e77301861bc0e389
Steven Hasegawa <steven@hsgws.com>
2018-05-23 08:59:30 -0700

    Added in content that Jason wrote and updated some color variables and references to those variables

--- packages/uikit/src/components/Link/styleguide.jsx
+++ packages/uikit/src/components/Link/styleguide.jsx
@@ -4,6 +4,7 @@ import React from 'react';
 import Link from '.';
 import { withControls } from '../../../styleguide/client/higherOrderComponents';
 import { boolean, select, text } from '../../../styleguide/client/knobs';
+import typography from '../../styles/typography.css';
 
 const typeOptions = [
   { text: 'Primary', value: 'primary' },
@@ -20,7 +21,8 @@ const knobs = {
 
 const LinkStyleguide = ({ content, to, type, button, disabled }) => (
   <div>
-    <h1>Link</h1>
+    <div className={typography.heading1}>Link</div>
+    <p>Links are used to take the user from one page to another.</p>
     <Link
       to={to}
       button={button}
@@ -29,6 +31,15 @@ const LinkStyleguide = ({ content, to, type, button, disabled }) => (
     >
       {content}
     </Link>
+    <div className={typography.heading3}>Style and usage</div>
+    <p>
+      Text links may be applied to body content elements and body-small content
+      elements, but generally are not applied to titles or headings. The
+      application of a link does not change the underlying type style, except by
+      the addition of color and an underline. (Links should not change an
+      item&apos;s type weight from &apos;regular&apos; to &apos;bold&apos;, for
+      example.)
+    </p>
   </div>
 );
 
