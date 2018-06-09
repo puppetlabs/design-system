bec606ec56deb938fbc78f2f8fbb71e8851c5776
Noah Muldavin <nmuldavin@gmail.com>
2018-06-08 21:31:11 -0700

    Create Icon component and first implementation
    There is now a subdirectory called icons in src. It will contain a jsx file for every
    icon. Each Icon is a React component that wraps a path and/or other elemnts in the Icon component.
    
    Principles:
    * No ids on svg elements.
    * Only put classnames on svg elements if they need to be individually styled. If so use imported, modularized css classnames like we've been doing for the rest of our components

---
+++ packages/uikit/styleguide/client/components/foundations/Icons/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import StyleguideContent from '../../StyleguideContent';
+import Header from '../../../../../src/components/Header';
+import ActionIcon from '../../../../../src/icons/Action';
+
+export default () => (
+  <StyleguideContent>
+    <div>
+      <Header>Icons</Header>
+      <ActionIcon />
+    </div>
+  </StyleguideContent>
+);
