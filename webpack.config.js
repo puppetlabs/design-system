2c9e119a5a4dd836fbde77e0097f6cc51a5fac4e
Colby Aley <colby@aley.me>
2018-06-18 17:07:04 -0700

    Specify plugins instead of mode in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,25 @@ const webpack = require('webpack');
 const path = require('path');
 
 const env = process.env.NODE_ENV || 'development';
+let plugins;
+
+// We want to manually specify these plugins, because using mode: production
+// includes uglify which we don't want. https://webpack.js.org/concepts/mode/#mode-production
+if (env === 'production') {
+  plugins = [
+    new webpack.DefinePlugin({ "process.env.NODE_ENV": JSON.stringify("production") }),
+    new webpack.optimize.ModuleConcatenationPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+  ];
+} else {
+  plugins = [
+    new webpack.NamedModulesPlugin(),
+    new webpack.DefinePlugin({ "process.env.NODE_ENV": JSON.stringify("development") }),
+  ];
+}
 
 module.exports = {
-  mode: env,
+  mode: 'none', // We don't want this feature. See above.
   entry: {
     library: './source/index.js',
   },
@@ -42,7 +58,5 @@ module.exports = {
       },
     }],
   },
-  plugins: [
-    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-  ],
+  plugins,
 };
