b42100d536a8d701736a20b5c957877711ade98b
sprokusk <sprokuski@gmail.com>
2018-06-22 14:57:22 -0700

    Further integrate Portal class with portal HOC

--- source/react/library/portal.js
+++ source/react/library/portal.js
@@ -5,51 +5,51 @@ import { APP_BASE } from '../constants';
 
 const propTypes = {
   isOpened: PropTypes.bool,
-  content: PropTypes.element,
 };
 
 const defaultProps = {
   isOpened: true,
-  content: null,
 };
 
 // This HOC wraps passed content in a native react portal.
-const portal = Base => props => (
-  <Portal content={ <Base { ...props } /> } { ...props } />
-);
+const portal = (Base) => {
+  class Portal extends React.Component {
+    constructor(props) {
+      super(props);
 
-class Portal extends React.Component {
-  constructor(props) {
-    super(props);
+      this.rootSelector = APP_BASE;
+      this.container = document.createElement('div');
 
-    this.rootSelector = APP_BASE;
-    this.container = document.createElement('div');
-
-    // This ensures that components that pre-render aren't appended to the DOM till acted on
-    // while components that render after an action is taken are appended immediately
-    if (props.isOpened) {
-      this.rootSelector.appendChild(this.container);
+      // This ensures that components that pre-render aren't appended to the DOM till acted on
+      // while components that render after an action is taken are appended immediately
+      if (props.isOpened) {
+        this.rootSelector.appendChild(this.container);
+      }
     }
-  }
 
-  componentWillReceiveProps(newProps) {
-    if (newProps.isOpened && !this.props.isOpened) {
-      this.rootSelector.appendChild(this.container);
-    } else if (!newProps.isOpened && this.props.isOpened) {
+    componentWillReceiveProps(newProps) {
+      if (newProps.isOpened && !this.props.isOpened) {
+        this.rootSelector.appendChild(this.container);
+      } else if (!newProps.isOpened && this.props.isOpened) {
+        this.rootSelector.removeChild(this.container);
+      }
+    }
+
+    componentWillUnmount() {
       this.rootSelector.removeChild(this.container);
     }
+
+    render() {
+      const base = <Base { ...this.props } />;
+
+      return ReactDOM.createPortal(base, this.container);
+    }
   }
 
-  componentWillUnmount() {
-    this.rootSelector.removeChild(this.container);
-  }
+  Portal.propTypes = propTypes;
+  Portal.defaultProps = defaultProps;
 
-  render() {
-    return ReactDOM.createPortal(this.props.content, this.container);
-  }
-}
-
-Portal.propTypes = propTypes;
-Portal.defaultProps = defaultProps;
+  return Portal;
+};
 
 export default portal;
