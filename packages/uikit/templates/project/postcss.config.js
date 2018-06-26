7675bd70cd7a746afb4695038d448171eaad2fcf
Noah Muldavin <nmuldavin@gmail.com>
2018-06-26 09:43:17 -0700

    Add autoprefixer and css minification in production

--- packages/uikit/templates/project/postcss.config.js
+++ packages/uikit/templates/project/postcss.config.js
@@ -1,20 +1,5 @@
-// const parseVariables = require('./utils/parseVariables');
-
-/**
- * Put parsed global css variables here
- */
-const variables = {};
+const autoprefixer = require('autoprefixer');
 
 module.exports = {
-  plugins: {
-    'postcss-preset-env': {
-      stage: 0,
-      features: {
-        'custom-properties': {
-          variables,
-          preserve: process.env.NODE_ENV !== 'production',
-        },
-      },
-    },
-  },
+  plugins: [autoprefixer],
 };
