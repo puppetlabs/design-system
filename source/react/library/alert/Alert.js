d7b29a572840c0482ac26f3b3aab1276630d2a42
Geoff Woodburn <geoff@woodburndesigns.com>
2018-09-21 15:44:49 -0700

    Adding custom alerts which allows us to better match the styleguide.

--- source/react/library/alert/Alert.js
+++ source/react/library/alert/Alert.js
@@ -1,3 +1,68 @@
-import Alert from 'react-notification';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import React from 'react';
+import Button from '../buttons/Button';
+import Text from '../text';
+import Icon from '../icon';
+
+const propTypes = {
+  message: PropTypes.string.isRequired,
+  isActive: PropTypes.bool,
+  type: PropTypes.oneOf(['success', 'error', 'info', 'warning']),
+  closeable: PropTypes.bool,
+  growl: PropTypes.bool,
+  onClose: PropTypes.func,
+};
+
+const defaultProps = {
+  isActive: false,
+  closeable: true,
+  type: 'success',
+  growl: true,
+  onClose: () => {},
+};
+
+const Alert = props => {
+  const { message, isActive, type, closeable, onClose, growl } = props;
+  const classNames = classnames('rc-alert', {
+    [`rc-alert-${type}`]: type,
+    'rc-alert-static': !growl,
+  });
+  let closeButton;
+  let typeIcon;
+
+  switch (type) {
+    case 'error':
+      typeIcon = 'close-circle';
+      break;
+    case 'info':
+    case 'warning':
+      typeIcon = 'info-circle';
+      break;
+    default:
+      typeIcon = 'check-circle';
+  }
+
+  if (!isActive) {
+    return false;
+  }
+
+  if (closeable) {
+    closeButton = <Icon className="rc-alert-close" type="close" size="tiny" />;
+  }
+
+  return (
+    <div className={classNames}>
+      <Icon className="rc-alert-type-icon" type={typeIcon} size="medium" />
+      <Text className="rc-alert-message" size="small">
+        {message}
+      </Text>
+      {closeButton}
+    </div>
+  );
+};
+
+Alert.propTypes = propTypes;
+Alert.defaultProps = defaultProps;
 
 export default Alert;
--- source/react/library/alert/Alert.md
+++ source/react/library/alert/Alert.md
@@ -1,5 +1,37 @@
-```
-const initialState = { isActive: false };
+## Alert as a growl
 
-<Button label="Show notification" />
+```
+const initialState = { isGrowlActive: false };
+<div>
+  <Button label="Toggle growl" onClick={ () => { setState({ isGrowlActive: !state.isGrowlActive }) } } />
+  <Alert
+    isActive={ state.isGrowlActive }
+    message="Hello world"
+    onClose={ () => { setState({ isGrowlActive: false }) } }
+  />
+</div>
+```
+
+## Alert types
+
+```
+  <Alert growl={ false } isActive={ true } message="Hello world" />
+```
+
+Error
+
+```
+<Alert growl={ false } isActive={ true } type="error" message="Hello world" />
+```
+
+Warning
+
+```
+<Alert growl={ false } isActive={ true } type="warning" message="Hello world" />
+```
+
+Info
+
+```
+<Alert growl={ false } isActive={ true } type="info" message="Hello world" />
 ```
