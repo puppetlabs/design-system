72f807fd7f17d33a0bce20600aeb1faf94190dca
Michael Sigler <mikesigler@gmail.com>
2018-10-02 15:08:14 -0700

    removed unused badge levels

--- source/react/library/badge/Badge.js
+++ source/react/library/badge/Badge.js
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 const propTypes = {
   type: PropTypes.oneOf(['bold', 'subtle', 'pill']),
   color: PropTypes.oneOf(['danger', 'info', 'neutral', 'success', 'warning']),
-  level: PropTypes.oneOf([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, null]),
+  level: PropTypes.oneOf([0, 1, 2, 3, 4, 5, 6, null]),
   palette: PropTypes.oneOf(['sequential', null]),
   className: PropTypes.string,
   children: PropTypes.node,
