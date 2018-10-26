006f72ab1913733400fb3d4d3a7680e11cbdc7ca
Nathan Ward <nathan.ward@puppet.com>
2018-10-26 13:45:49 -0700

    Allow update-styleguide.sh script to be called from anywhere

--- design/update-styleguide.sh
+++ design/update-styleguide.sh
@@ -1,2 +1,3 @@
-rm -rf styleguide/images
-$(mdfind kMDItemCFBundleIdentifier == 'com.bohemiancoding.sketch3' | head -n 1)/Contents/Resources/sketchtool/bin/sketchtool export artboards puppet-styleguide.sketch --output=styleguide/images --scales=2
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+rm -rf "$DIR/styleguide/images"
+$(mdfind kMDItemCFBundleIdentifier == 'com.bohemiancoding.sketch3' | head -n 1)/Contents/Resources/sketchtool/bin/sketchtool export artboards "$DIR/puppet-styleguide.sketch" --output="$DIR/styleguide/images" --scales=2
