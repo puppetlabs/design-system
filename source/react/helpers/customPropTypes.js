730cdf5a5cc003dc181839bfbf94496bcd853f59
Noah Muldavin <nmuldavin@gmail.com>
2018-11-28 16:46:10 -0800

    Document prop types

--- source/react/helpers/customPropTypes.js
+++ source/react/helpers/customPropTypes.js
@@ -1,10 +1,18 @@
 import PropTypes from 'prop-types';
 
+/**
+ * PropType for anything that can be rendered as a JSX element. Most often this
+ * will be used for the flexible element rendering 'as' prop. See library/card/Card.js
+ * for an example.
+ */
 export const renderableElement = PropTypes.oneOfType([
   PropTypes.string,
   PropTypes.func,
 ]);
 
+/**
+ * Design system available element elevations
+ */
 export const elementElevation = PropTypes.oneOf([
   0,
   50,
