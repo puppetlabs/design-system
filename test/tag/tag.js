a8fea7591d455186c9d6bae889c9aa046cc9fe67
sprokusk <sprokuski@gmail.com>
2018-12-06 13:48:29 -0800

    Add tests for Tag click events

--- test/tag/tag.js
+++ test/tag/tag.js
@@ -1,6 +1,7 @@
 import jsdom from 'mocha-jsdom';
 import { shallow } from 'enzyme';
 import { expect } from 'chai';
+import sinon from 'sinon';
 import React from 'react';
 
 import Tag from '../../source/react/library/tag/Tag';
@@ -18,5 +19,27 @@ describe('<Tag />', () => {
     const wrapper = shallow(<Tag tooltip>hi!</Tag>);
 
     expect(wrapper.find('TooltipHoverArea').length).to.eql(1);
+  });
+
+  it('should respond to a click event', () => {
+    const onClick = sinon.spy();
+    const wrapper = shallow(<Tag onClick={onClick} />);
+
+    wrapper.simulate('click', { preventDefault() {} });
+
+    expect(onClick.called).to.equal(true);
+  });
+
+  it('should respond to a separate, isolated onRemove click event', () => {
+    const onClick = sinon.spy();
+    const onRemove = sinon.spy();
+    const wrapper = shallow(<Tag onClick={onClick} onRemove={onRemove} />);
+
+    wrapper
+      .find('.rc-tag-remove-button')
+      .simulate('click', { preventDefault() {}, stopPropagation() {} });
+
+    expect(onClick.called).to.equal(false);
+    expect(onRemove.called).to.equal(true);
   });
 });
