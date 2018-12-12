be9959c7e5a46cc5963beb6ec03dc701211cfb1e
sprokusk <sprokuski@gmail.com>
2018-12-11 16:48:19 -0800

    Tighten sidebar scroll and refine styles

--- source/react/constants.js
+++ source/react/constants.js
@@ -8,7 +8,7 @@ export const RIGHT_KEY_CODE = 39;
 export const UP_KEY_CODE = 38;
 export const DOWN_KEY_CODE = 40;
 
-export const SIDEBAR_SUBSECTION_TRUNC_LENGTH = 7;
+export const SIDEBAR_SUBSECTION_TRUNC_LENGTH = 6;
 
 export const filterOperators = [
   { symbol: '=', label: 'Equals', sentence: 'is equal to' },
