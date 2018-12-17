478b4a46d49f379249908eb333454b564e03b5c2
sprokusk <sprokuski@gmail.com>
2018-12-17 00:57:37 -0800

    Use props, not component state for controlling filter form cta

--- source/react/library/filters/FilterForm.js
+++ source/react/library/filters/FilterForm.js
@@ -225,8 +225,7 @@ class FilterForm extends React.Component {
     const operators = this.getOperators();
     const fields = this.getFields();
 
-    const { strings, size, actionsPosition, cancellable } = this.props;
-    const { filter } = this.state;
+    const { strings, size, actionsPosition, cancellable, filter } = this.props;
 
     const submitLabel = Object.keys(filter).length
       ? strings.filterUpdate
