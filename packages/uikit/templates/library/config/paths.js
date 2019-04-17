9cdb7c9e03c7476f87b3316b10c2a7e9580a1a09
Nathan Ward <nathan.ward@puppet.com>
2019-04-17 11:21:29 -0700

    Add library template
    This is mostly taken from react-layouts

---
+++ packages/uikit/templates/library/config/paths.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const { map } = require('ramda');
+
+const paths = map(p => path.resolve.bind(null, __dirname, '..', p), {
+  root: '',
+  src: 'src',
+  dist: 'dist',
+});
+
+module.exports = paths;
