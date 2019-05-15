5626c13cbc1ffbb27a71b3d8638aaa598345bc85
sprokusk <sprokuski@gmail.com>
2019-05-15 15:04:40 -0700

    Add prop definitions and tests for Badge

--- source/react/library/badge/Badge.js
+++ source/react/library/badge/Badge.js
@@ -3,11 +3,17 @@ import classNames from 'classnames';
 import PropTypes from 'prop-types';
 
 const propTypes = {
+  /** Type dictates badge coloring */
   type: PropTypes.oneOf(['danger', 'info', 'neutral', 'success', 'warning']),
+  /** Weight dictates the punchiness of badge coloring */
   weight: PropTypes.oneOf(['bold', 'subtle']),
+  /** Setting pill to true fully rounds the borders */
   pill: PropTypes.bool,
+  /** The content to render within the badge */
   children: PropTypes.node,
+  /** Optional additional classnames */
   className: PropTypes.string,
+  /** Optional additional inline styles */
   style: PropTypes.shape({}),
 };
 
