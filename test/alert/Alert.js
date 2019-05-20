88f07c765063b72a55c3dfd6a545b666ffc4fdda
sprokusk <sprokuski@gmail.com>
2019-05-20 16:01:08 -0700

    Add tests for Alert

--- test/alert/Alert.js
+++ test/alert/Alert.js
@@ -1,4 +1,5 @@
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
+import sinon from 'sinon';
 import { expect } from 'chai';
 import React from 'react';
 
@@ -6,8 +7,58 @@ import Alert from '../../source/react/library/alert/Alert';
 
 describe('<Alert />', () => {
   it('should render without blowing up', () => {
-    const wrapper = shallow(<Alert message="i'm an alert" />);
+    const wrapper = shallow(<Alert />);
 
     expect(wrapper.length).to.eql(1);
+  });
+
+  it('applies provided classNames to the outer wrapper', () => {
+    const className = 'foo';
+    expect(shallow(<Alert className={className} />)).to.have.className(
+      className,
+    );
+  });
+
+  it('applies provided inline styles to the outer wrapper', () => {
+    const style = { hidden: true };
+    expect(shallow(<Alert style={style} />)).to.have.to.have.style(style);
+  });
+
+  it('should render the children provided', () => {
+    const message = 'hello world!';
+    expect(mount(<Alert>{message}</Alert>).find('Text')).to.have.text(message);
+  });
+
+  it('adds a new class for the designated type', () => {
+    const success = 'success';
+    const danger = 'danger';
+
+    expect(
+      shallow(<Alert type={success}>hello world!</Alert>),
+    ).to.have.className(`rc-alert-${success}`);
+
+    expect(
+      shallow(<Alert type={danger}>hello world!</Alert>),
+    ).to.have.className(`rc-alert-${danger}`);
+  });
+
+  it('adds a new class if elevated prop is provided', () => {
+    expect(shallow(<Alert elevated>hello world!</Alert>)).to.have.className(
+      `rc-alert-elevated`,
+    );
+  });
+
+  it('should render a functional dismissal button if provided an onClose', () => {
+    const onClose = sinon.spy();
+    const wrapper = mount(<Alert />);
+
+    expect(wrapper.find('IconButton').exists()).to.equal(false);
+
+    wrapper.setProps({ onClose });
+
+    wrapper.find('IconButton').simulate('click');
+
+    // eslint-disable-next-line
+    expect(onClose).to.have.been.called;
   });
 });
