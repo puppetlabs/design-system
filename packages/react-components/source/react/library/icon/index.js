16612330da82f92a3a92136576c6b039546a94c3
Nathan Ward <nathan.ward@puppet.com>
2019-09-20 14:14:06 -0700

    Export array of icons as Icon.icons

--- packages/react-components/source/react/library/icon/index.js
+++ packages/react-components/source/react/library/icon/index.js
@@ -1,3 +1,7 @@
 import Icon from './Icon';
+import icons from './icons';
+
+// This gives consumers access to the list of icons, e.g. for use in other styleguides
+Icon.icons = icons;
 
 export default Icon;
