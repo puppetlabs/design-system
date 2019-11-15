7226f056f86dc6b39e341f6cf9728a59739a1b28
Noah Muldavin <nmuldavin@gmail.com>
2019-11-15 14:29:45 -0800

    Fix tests

--- packages/uikit/bin/getNameVariants/getNameVariants.js
+++ packages/uikit/bin/getNameVariants/getNameVariants.js
@@ -15,35 +15,19 @@ const {
  * e.g. my-component => ['my', 'component'];
  *      myComponent => ['my', 'component'];
  */
-const parseName = pipe(
-  split(/-|_| |(?=[A-Z])/),
-  map(toLower),
-);
+const parseName = pipe(split(/-|_| |(?=[A-Z])/), map(toLower));
 
 const capitalizeWords = map(name => concat(toUpper(head(name)), tail(name)));
 
-const humanize = pipe(
-  parseName,
-  capitalizeWords,
-  join(' '),
+const humanize = pipe(parseName, capitalizeWords, join(' '));
+
+const pascalize = pipe(parseName, capitalizeWords, join(''));
+
+const camelize = pipe(parseName, parsedName =>
+  concat(head(parsedName), capitalizeWords(tail(parsedName)).join('')),
 );
 
-const pascalize = pipe(
-  parseName,
-  capitalizeWords,
-  join(''),
-);
-
-const camelize = pipe(
-  parseName,
-  parsedName =>
-    concat(head(parsedName), capitalizeWords(tail(parsedName)).join('')),
-);
-
-const dasherize = pipe(
-  parseName,
-  join('-'),
-);
+const dasherize = pipe(parseName, join('-'));
 
 const getNameVariants = original => ({
   original,
