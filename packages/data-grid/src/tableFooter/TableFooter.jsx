1bd7d5543893fb4b72fc519f74f467d4564d0165
rhyza <rhyza.velasco@puppet.com>
2019-12-11 14:29:57 -0800

    Internationalize row count in footer component

--- packages/data-grid/src/tableFooter/TableFooter.jsx
+++ packages/data-grid/src/tableFooter/TableFooter.jsx
@@ -4,27 +4,27 @@ import { Text } from '@puppet/react-components';
 import './TableFooter.scss';
 
 const propTypes = {
-  /** Optional feature to display number of rows in table */
-  rowCount: PropTypes.shape({
-    /** Provide the number of rows displayed */
-    count: PropTypes.string,
-    /** Optional change label displayed after count. Current default "rows" */
-    label: PropTypes.string,
-  }),
+  /** Optional feature to display number of rows in table. Provide both the count and 'item' label in a string. */
+  rowCount: PropTypes.string,
+  /** Optional feature to display number of rows selected in table. Provide both the count and 'selected' label in a string. */
+  selectedRowCount: PropTypes.string,
   children: PropTypes.node,
 };
 
 const defaultProps = {
-  rowCount: {},
+  rowCount: null,
+  selectedRowCount: null,
   children: undefined,
 };
 
-function TableFooter({ children, rowCount }) {
+function TableFooter({ children, rowCount, selectedRowCount }) {
   return (
     <div className="dg-table-footer-container">
       {children === undefined ? (
         <Text as="h3" color="medium" className="dg-table-row-count">
-          {rowCount.count} {rowCount.label ? rowCount.label : null}
+          {rowCount || null}
+          {rowCount && selectedRowCount ? ' - ' : null}
+          {selectedRowCount || null}
         </Text>
       ) : (
         children
