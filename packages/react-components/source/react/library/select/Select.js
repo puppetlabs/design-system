b453bcfd82bd0e16054069a8313d66514177faef
rhyza <rhyza.velasco@puppet.com>
2020-01-28 10:09:27 -0800

    Expand prop def for Select and ButtonSelect

--- packages/react-components/source/react/library/select/Select.js
+++ packages/react-components/source/react/library/select/Select.js
@@ -30,6 +30,8 @@ const propTypes = {
       label: PropTypes.string.isRequired,
       /** Optional icon associated with this option */
       icon: PropTypes.oneOf(Icon.AVAILABLE_ICONS),
+      /** Optional custom icon associated with this option */
+      svg: PropTypes.element,
     }),
   ),
   /** Currently selected value or values */
