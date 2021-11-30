481aad0b7ed0a26358837defa464ddf0b146916c
Kyle Rable <Krable55@gmail.com>
2021-11-30 15:48:39 -0800

    Use popper.js library for tooltip

--- packages/react-components/source/react/library/tooltips/TooltipHoverArea.js
+++ packages/react-components/source/react/library/tooltips/TooltipHoverArea.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { Suspense, lazy, useState, useEffect, useRef } from 'react';
+import { createPortal } from 'react-dom';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import { usePopper } from 'react-popper';
 
 export const propTypes = {
   /** Position of tooltip relative to the activating element */
@@ -17,6 +19,10 @@ export const propTypes = {
   style: PropTypes.shape({}),
   /** Optional, prevents tooltip from displaying when false */
   enabled: PropTypes.bool,
+  //Add popper flip option described in https://popper.js.org/popper-documentation.html#modifiers
+  flip: PropTypes.bool,
+  //Add popper arrow option described in https://popper.js.org/popper-documentation.html#modifiers
+  arrow: PropTypes.bool,
 };
 
 export const defaultProps = {
@@ -25,6 +31,8 @@ export const defaultProps = {
   style: {},
   enabled: true,
   onClick: undefined,
+  flip: true,
+  arrow: true,
 };
 
 /**
@@ -43,22 +51,108 @@ const TooltipHoverArea = ({
   onClick,
   style,
   enabled,
+  ...popperOptions
 }) => {
+  const arrowRef = useRef(null);
+  const [referenceElement, setReferenceElement] = useState(null);
+  const [popperElement, setPopperElement] = useState(null);
+
+  const popperModifiers = [
+    {
+      name: 'flip',
+      enabled: popperOptions.flip,
+    },
+    {
+      name: 'arrow',
+      enabled: popperOptions.arrow,
+      options: {
+        element: arrowRef?.current,
+        padding: 1,
+        offset: [0, 6],
+      },
+    },
+    {
+      name: 'offset',
+      options: {
+        offset: [0, 6],
+      },
+    },
+    {
+      name: 'preventOverflow',
+      options: {
+        padding: 0,
+      },
+    },
+    {
+      name: 'hide',
+      enabled: !enabled,
+    },
+  ];
+
+  const { styles, attributes } = usePopper(referenceElement, popperElement, {
+    placement: anchor,
+    modifiers: popperModifiers,
+  });
+
+  const handleMouseEvent = event => () => {
+    if (event === 'mouseover') {
+      popperElement?.setAttribute('data-show', '');
+    } else {
+      popperElement?.removeAttribute('data-show');
+    }
+  };
+
+  const addListeners = () => {
+    ['mouseover', 'mouseout'].forEach(event => {
+      referenceElement?.addEventListener?.(
+        event,
+        handleMouseEvent(event),
+        true,
+      );
+    });
+  };
+  const removeListeners = () => {
+    ['mouseover', 'mouseout'].forEach((event, i) => {
+      referenceElement?.removeEventListener?.(
+        event,
+        handleMouseEvent(event),
+        true,
+      );
+    });
+  };
+
+  useEffect(() => {
+    addListeners();
+    return () => removeListeners();
+  }, [referenceElement]);
+
+  const root = document.getElementsByClassName('app')[0];
+
   return (
-    <span
-      aria-hidden="true"
-      className={classNames(`rc-tooltip-container`, className)}
-      onClick={onClick}
-      onKeyPress={onClick}
-      style={{ ...style }}
-    >
-      {enabled && !!children && !!tooltip && (
-        <div className={classNames('rc-tooltip', `rc-tooltip-${anchor}`)}>
-          {tooltip}
-        </div>
-      )}
-      {children}
-    </span>
+    <>
+      {!!children &&
+        !!tooltip &&
+        createPortal(
+          <div
+            className={classNames('rc-tooltip', className)}
+            ref={setPopperElement}
+            style={styles.popper}
+            {...attributes.popper}
+          >
+            {popperOptions.arrow && (
+              <span
+                className="rc-tooltip-arrow"
+                style={styles.arrow}
+                {...attributes.arrow}
+                ref={arrowRef}
+              />
+            )}
+            {tooltip}
+          </div>,
+          root,
+        )}
+      {React.cloneElement(children, { ref: setReferenceElement })}
+    </>
   );
 };
 
