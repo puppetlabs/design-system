dfd6f2e428d8c6e646dfc8c2ad48de365c20c901
krable55@gmail.com <krable55@gmail.com>
2023-02-06 21:04:02 -0800

    Bump & lint

--- packages/react-components/source/react/library/menu/Arrow.js
+++ packages/react-components/source/react/library/menu/Arrow.js
@@ -2,14 +2,14 @@
 import React from 'react';
 
 // eslint-disable-next-line no-shadow
-const Arrow = ({ arrowRef, className, style, attributes }) => {
+const Arrow = ({ arrowRef, style, attributes }) => {
   const { transform, ...rest } = style;
   const rotate = `${transform} rotate(45deg)`;
 
   return (
     <span
       id="rc-popup-menu-arrow"
-      className={className}
+      className="rc-popup-menu-arrow"
       style={{ transform: rotate, ...rest }}
       {...attributes}
       ref={arrowRef}
